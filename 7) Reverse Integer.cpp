3019ae653066b5159438fa29737b817e1ff4cb14
Shivaanjay Narula <115805065+Shivaanjay@users.noreply.github.com>
2024-09-23 02:39:00 +0530

    Create 7) Reverse Integer.cpp

---
+++ 7) Reverse Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long n=0;
+        while(x)
+        {
+            n = n*10 + x%10;
+            x = x/10;
+        }
+        if(n > INT_MAX || n < INT_MIN)
+        {
+            return 0;
+        }
+        return n;
+    }
+};
