2957dce05481bf00647cb1f7e568537fa15af84d
Shivaanjay Narula <115805065+ShivaanjayNarula@users.noreply.github.com>
2025-01-22 20:27:59 +0530

    Create 2849) Determine if a Cell Is Reachable at a Given Time.cpp

---
+++ 2849) Determine if a Cell Is Reachable at a Given Time.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int x = abs(sx - fx);
+        int y = abs(sy - fy);
+        if(x == 0 && y == 0)
+        {
+            return t != 1;
+        }
+        if(x <= t && y <= t)
+        {
+            return true;
+        }
+        return false;
+    }
+};
