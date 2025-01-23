1e103c3a662f86579b395ace94140039cd717d41
Shivaanjay Narula <115805065+ShivaanjayNarula@users.noreply.github.com>
2025-01-24 01:22:41 +0530

    Create 0892) Surface Area of 3D Shapes.cpp

---
+++ 0892) Surface Area of 3D Shapes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int surfaceArea(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j])
+                {
+                    ans += grid[i][j] * 4 + 2;
+                }
+                if(i)
+                {
+                    ans -= min(grid[i][j], grid[i-1][j]) * 2;
+                }
+                if(j)
+                {
+                    ans -= min(grid[i][j], grid[i][j-1]) * 2;
+                }
+            }
+        }
+        return ans;
+    }
+};
