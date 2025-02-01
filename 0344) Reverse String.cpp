49097629185d5a60deb8bc04c2646dad13245ba2
Shivaanjay Narula <115805065+ShivaanjayNarula@users.noreply.github.com>
2025-02-01 05:34:35 +0530

    Create 0344) Reverse String.cpp

---
+++ 0344) Reverse String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+      int i = 0;
+        int n = s.size();
+        while(i < n/2)
+        {
+          swap(s[i], s[n - i -1]);
+          i++;
+        }
+    }
+};
