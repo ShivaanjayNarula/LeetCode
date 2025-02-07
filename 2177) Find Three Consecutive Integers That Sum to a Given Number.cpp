8baf17afe3ab0324c0220f15aec4446b24062945
Shivaanjay Narula <115805065+ShivaanjayNarula@users.noreply.github.com>
2025-02-07 06:42:33 +0530

    Create 2177) Find Three Consecutive Integers That Sum to a Given Number.cpp

---
+++ 2177) Find Three Consecutive Integers That Sum to a Given Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        vector<long long> ans;
+        if(num % 3 != 0)
+        {
+            return ans;
+        }
+        long long mid = num/3;
+        ans.push_back(mid-1);
+        ans.push_back(mid);
+        ans.push_back(mid+1);
+        return ans;
+    }
+};
