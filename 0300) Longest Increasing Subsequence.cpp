6c47da59d42f72cad599566b720e305ff3b20ec7
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-04-30 04:42:58 +0530

    Create 0300) Longest Increasing Subsequence.cpp

---
+++ 0300) Longest Increasing Subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if(nums.empty())
+        {
+            return 0;
+        }
+        vector<int> ans(n, 1);
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    ans[i] = max(ans[i], ans[j] + 1);
+                }
+            }
+        }
+        return *max_element(ans.begin(), ans.end());
+    }
+};
