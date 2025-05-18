fab73375d159eb98793a9fe0f0adb7b60638cee9
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-05-19 00:53:25 +0530

    Create 0148) Sort List.cpp

---
+++ 0148) Sort List.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> nums;
+        ListNode *temp = head;
+        while(temp)
+        {
+            nums.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(nums.begin(), nums.end());
+        temp = head;
+        int check = 0;
+        while(temp)
+        {
+            temp->val = nums[check];
+            temp = temp->next;
+            check++;
+        }
+        return head;
+    }
+};
