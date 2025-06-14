511ec4ab5c48cb28dd31ff2718713d12502ffd7a
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-06-15 00:19:29 +0530

    Create 1574) Shortest Subarray to be Removed to Make Array Sorted.cpp

---
+++ 1574) Shortest Subarray to be Removed to Make Array Sorted.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int left = 0;
+        int right = n-1;
+        while(left + 1 < n && arr[left] <= arr[left+1])
+        {
+            left++;
+        }
+        if(left == n-1)
+        {
+            return 0;
+        }
+        while(right > 0 && arr[right-1] <= arr[right])
+        {
+            right--;
+        }
+        int ans = min(n - left - 1, right);
+        int i = 0; 
+        while(i <= left && right < n)
+        {
+            if(arr[i] <= arr[right])
+            {
+                ans = min(ans, right - i - 1);
+                i++;
+            }
+            else
+            {
+                right++;
+            }
+        }
+        return ans;
+    }
+};
