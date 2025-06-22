8fca834a6d3291726316884417676b000ee5a6dd
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-06-22 14:43:39 +0530

    Create 0078) Subsets.cpp

---
+++ 0078) Subsets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans = {{}};
+        for(auto i : nums)
+        {
+            int n = ans.size();
+            for(int j = 0; j < n; j++)
+            {
+                ans.push_back(ans[j]);
+                ans.back().push_back(i);
+            }
+        }
+        return ans;
+    }
+};
