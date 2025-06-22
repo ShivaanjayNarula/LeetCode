1e1a61b2659af23dc325c0cf52d3e4ca0a5cdb2a
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-06-22 14:42:51 +0530

    Create 0575) Distribute Candies.cpp

---
+++ 0575) Distribute Candies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n = candyType.size();
+        set<int> st(candyType.begin(), candyType.end());
+        if(st.size() > n/2)
+        {
+            return n/2;
+        }
+        return st.size();
+    }
+};
